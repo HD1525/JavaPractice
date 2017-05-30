bfff6688ddc9032d4e7598c03500ca9b942edf1e
add74 <add74@DESKTOP-LS45M5V>
2017-05-30 12:03:20 +0900

    실습과제 14

---
+++ JavaPractice/src/org/dimigo/exception/Movie.java
@@ -0,0 +1,44 @@
+/**
+ * 
+ */
+package org.dimigo.exception;
+
+/**
+ * <pre>
+ * org.dimigo.exception
+ *   |_ Movie
+ *
+ * 1. 개요 :
+ * 2. 작성일 : 2017. 5. 29.
+ * </pre>
+ *
+ * @author : add74
+ * @version : 1.0
+ */
+public class Movie {
+	private String title;
+	private int limitAge;
+	
+	public Movie(String title,int limitAge){
+		this.title=title;
+		this.limitAge=limitAge;
+	}
+	
+	public String getTitle(){
+		return title;
+	}
+	public int getLimitAge(){
+		return limitAge;
+	}
+	public void buyTicket(int age){
+		try{
+			if(age<limitAge){
+				throw new AgeCheckException(this);
+			}
+			else System.out.println(title+" 즐감하세요");
+		}
+		catch(Exception e){
+			System.out.println(e.getMessage());
+		}
+	}
+}
